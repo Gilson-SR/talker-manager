1e6c1a0760c3d1fa66aee24fe829e6e894230423
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-12-14 22:17:12 -0300

    fix: Requisito 5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const crypto = require('crypto');
-const validateLogin = require('./middlewares/validateLogin');
+const { validateLogin } = require('./middlewares/validateLogin');
+const { validateAge } = require('./middlewares/validateAge');
+const { validateAuthentication } = require('./middlewares/validateAuthentication');
+const { validateName } = require('./middlewares/validateName');
+const { validateTalk } = require('./middlewares/validateTalk');
 
 const app = express();
 app.use(express.json());
@@ -34,6 +38,17 @@ const newToken = () => crypto.randomBytes(8).toString('hex');
 app.post('/login', validateLogin, (_req, res) => {
   res.status(200).json({ token: newToken() });
 });
+
+app.post('/talker',
+validateAuthentication, validateName, validateAge, validateTalk,
+  (req, res) => {
+    const { name, age, talk } = req.body;
+    const talkers = JSON.parse(fs.readFileSync('src/talker.json'));
+    const addTalk = { id: talkers.length + 1, name, age, talk };
+    talkers.push(addTalk);
+    fs.writeFileSync('src/talker.json', JSON.stringify(talkers));
+    res.status(201).json(addTalk);
+  });
 
 app.listen(PORT, () => {
   console.log('Online');
